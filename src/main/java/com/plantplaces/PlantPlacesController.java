5187da28e861495180b170541d2bcc533a637e3b
ochibaru <ginaaaahop@gmail.com>
2019-09-24 14:33:01 -0400

    Pushing to sandbox

--- src/main/java/com/plantplaces/PlantPlacesController.java
+++ src/main/java/com/plantplaces/PlantPlacesController.java
@@ -1,7 +1,9 @@
 package com.plantplaces;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 
 /*@author Administrator
@@ -10,14 +12,24 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class PlantPlacesController {
 
-	@RequestMapping("/index")
-	public String start() {
+	@RequestMapping(value="/start", method=RequestMethod.GET)
+	public String read() {
 		
 		return "start";
 	}
 	
+	@PostMapping("/start")
+	public String create() {
+		
+		return "start";
+	}	
+	/*
+	 * Handle the / endpoints
+	 * @return
+	 */
+	
 	@RequestMapping("/")
-	public String start() {
+	public String index() {
 		
 		return "start";
 	}
