8765cc577b137f0f48facd2322123dceb19a25ac
ochibaru <ginaaaahop@gmail.com>
2019-10-15 15:04:58 -0400

    Adding DTOs.

--- src/main/java/com/plantplaces/PlantPlacesController.java
+++ src/main/java/com/plantplaces/PlantPlacesController.java
@@ -2,9 +2,11 @@ package com.plantplaces;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.plantplaces.dto.SpecimenDTO;
 import com.plantplaces.service.ISpecimenService;
@@ -21,8 +23,9 @@ public class PlantPlacesController {
 	private ISpecimenService specimenServiceStub;
 
 	@RequestMapping(value="/start", method=RequestMethod.GET)
-	public String read() {
+	public String read(Model model) {
 		SpecimenDTO specimenDTO = specimenServiceStub.fetchById(43);
+		model.addAttribute("specimenDTO", specimenDTO);
 		
 		return "start";
 	}
@@ -40,9 +43,13 @@ public class PlantPlacesController {
 	}
 	
 	@RequestMapping(value="/start", method=RequestMethod.GET, params= {"loyalty=silver"})
-	public String readSilver() {
-		
-		return "start";
+	public ModelAndView readSilver() {
+		ModelAndView modelAndView = new ModelAndView();
+		SpecimenDTO specimenDTO = specimenServiceStub.fetchById(43);
+		specimenDTO.setSpecimenId(90);
+		modelAndView.setViewName("start");
+		modelAndView.addObject("specimenDTP", specimenDTO);
+		return modelAndView;
 	}
 	
 	@PostMapping("/start")
